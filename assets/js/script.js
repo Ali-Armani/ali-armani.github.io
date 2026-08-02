675492d02cb9c1e5e9600609d0a2dc4f52c8e48d
Ali Armani <armani.ali@proton.me>
2026-08-02 21:14:11 +0330

    feat: add overlay to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 
 const btn = document.getElementById('menu-button');
+const overlay = document.getElementById('overlay');
 
 btn.addEventListener('click', navToggle);
 
 function navToggle() {
     btn.classList.toggle('open');
+    overlay.classList.toggle('overlay-show');
 }
